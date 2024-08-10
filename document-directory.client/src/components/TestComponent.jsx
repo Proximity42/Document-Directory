1e4a9bb5e4c7d0daad4384cf112e91da7d275c20
Владислав Костромин <vlad_kostromin@list.ru>
2024-08-10 09:03:56 +0700

    Создан файл docker-compose.yml

---
+++ document-directory.client/src/components/TestComponent.jsx
@@ -0,0 +1,9 @@
+import { NavLink } from 'react-router-dom';
+
+function TestComponent() {
+    return (
+        <a><NavLink to="/test">Переход</NavLink></a>
+    );
+}
+
+export default TestComponent;
