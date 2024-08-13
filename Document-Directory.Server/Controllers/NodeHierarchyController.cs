83097cceb209c30e73472e1ffea2b3752f18dd6b
Red Phantom <podverbny2013@gmail.com>
2024-08-13 22:00:35 +0700

    Отображение доступных папок/документов пользователю/группе

--- Document-Directory.Server/Controllers/NodeHierarchyController.cs
+++ Document-Directory.Server/Controllers/NodeHierarchyController.cs
@@ -1,8 +1,11 @@
-﻿using Document_Directory.Server.Models;
+﻿using Document_Directory.Server.Function;
+using Document_Directory.Server.Models;
 using Document_Directory.Server.ModelsDB;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace Document_Directory.Server.Controllers
 {
@@ -18,9 +21,13 @@ namespace Document_Directory.Server.Controllers
         }
 
         [HttpGet]
-        public async Task GetNodeHierarchies() //Отображает внешние папки, т.е. папки и документы, которые не вложены в другие папки
+        public async Task GetNodeHierarchies(int idUsers) //Отображает внешние папки, т.е. папки и документы, которые не вложены в другие папки
         {
             List<int> nodesId = new List<int>();
+
+            List<Groups> groupsUser = Functions.UserGroups(idUsers, _context);
+            List<int> idGroups = new List<int>();
+            foreach (var group in groupsUser) { idGroups.Add(group.Id); }
 
             foreach (NodeHierarchy folder in _context.NodeHierarchy) 
             {
@@ -28,10 +35,27 @@ namespace Document_Directory.Server.Controllers
             }
 
             List<Nodes> exFolders = (from Node in _context.Nodes where !nodesId.Contains(Node.Id) select Node).ToList();
+            List<NodeAccess> nodeAccesses = (from Node in _context.NodeAccess where (idGroups.Contains(Node.GroupId)) || (Node.UserId == idUsers) select Node).ToList();
+            
+            List<Nodes> nodes = new List<Nodes>();
+
+            foreach (var nodeAccess in nodeAccesses)
+            {
+                Nodes node = _context.Nodes.FirstOrDefault(n => n.Id == nodeAccess.NodeId);
+                nodes.Add(node);
+            }
+
+            List<Nodes> exFoldersTemp = new List<Nodes>(exFolders);
+
+            foreach (var node in exFolders)
+            {
+                if (nodes.Contains(node)) { continue; }
+                else { exFoldersTemp.Remove(node); }
+            }
 
             var response = this.Response;
             response.StatusCode = 200;
-            await response.WriteAsJsonAsync(exFolders);
+            await response.WriteAsJsonAsync(exFoldersTemp);
         }
 
         [HttpGet("{id}")]
